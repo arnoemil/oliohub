c3edc50095ca7996a8919b67b9d935f444e620cc
Arno Törö <arno.toro@gmail.com>
2022-01-12 20:04:14 +0200

    viikko2 tehtävä 1

---
+++ Viikko2/Dog.java
@@ -0,0 +1,12 @@
+package Viikko2;
+
+public class Dog {
+        
+    public Dog(String name) {
+        System.out.println("Hey, my name is " + name + "!");
+    }
+
+    public static void speak(String sentence) {
+        System.out.println(sentence);
+    }
+}
---
+++ Viikko2/Mainclass.java
@@ -0,0 +1,11 @@
+package Viikko2;
+
+public class Mainclass {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Rekku");
+        Dog dog2 = new Dog("Musti");
+
+        Dog.speak("Hau!");
+        Dog.speak("Vuh!");
+    }
+}
