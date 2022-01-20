b5d168951c202d3cd69dbc7165e22ab08eaf569f
Arno Törö <arno.toro@gmail.com>
2022-01-20 16:53:07 +0200

    viikko 2 teht 1

--- Viikko2/Mainclass.java
+++ Viikko2/Mainclass.java
@@ -1,6 +1,6 @@
 package Viikko2;
 
-public class Mainclass {
+class Mainclass {
     public static void main(String[] args) {
         Dog dog1 = new Dog("Rekku");
         Dog dog2 = new Dog("Musti");
