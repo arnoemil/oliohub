d1cad1b0d6f27ffad99ae9bc957bb9b481c4fbce
Arno Törö <arno.toro@gmail.com>
2022-02-04 14:24:30 +0200

    Viikko4

---
+++ Viikko4/Mainclass.java
@@ -0,0 +1,10 @@
+package Viikko4;
+
+public class Mainclass {
+    
+    public static void main(String[] args) {
+        ReadAndWriteIO io1 = new ReadAndWriteIO();
+        io1.readAndWrite("input.txt",
+        "output.txt");
+    }
+}
