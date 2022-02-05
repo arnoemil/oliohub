93be87ae8cd472ce976e62579dae260f43b6a0df
Arno Törö <arno.toro@gmail.com>
2022-02-05 14:11:13 +0200

    viikko 4

--- Viikko4/Mainclass.java
+++ Viikko4/Mainclass.java
@@ -4,7 +4,8 @@ public class Mainclass {
     
     public static void main(String[] args) {
         ReadAndWriteIO io = new ReadAndWriteIO();
-        io.readAndWrite("input.txt",
-        "output.txt");
+        /*io.readAndWrite("/home/arnoemil/Documents/oliohub/Viikko4/input.txt",
+        "output.txt");*/
+        io.readZip("/home/arnoemil/Documents/oliohub/Viikko4/zipinput.zip");
     }
 }
--- Viikko4/ReadAndWriteIO.java
+++ Viikko4/ReadAndWriteIO.java
@@ -1,10 +1,13 @@
 package Viikko4;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.zip.ZipInputStream;
 
 public class ReadAndWriteIO {
     
@@ -19,22 +22,43 @@ public class ReadAndWriteIO {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+    } 
 
     public void readAndWrite(String rfile, String wfile) {
-
         try {
             BufferedReader rf = new BufferedReader(new FileReader(rfile));
             BufferedWriter wf = new BufferedWriter(new FileWriter(wfile));
             String output;
             while((output = rf.readLine()) != null) {
-                wf.write(output);                
+                if (output.trim().length() > 1 && output.trim().length() < 30) { 
+                    if(output.trim().contains("v")) {
+                        wf.write(output+"\n");  
+                    }
+                }              
             }
             rf.close();
             wf.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    } 
+
+    public void readZip(String fname) {
+        try {
+            BufferedInputStream bs = new BufferedInputStream(new FileInputStream(fname));
+            ZipInputStream zs = new ZipInputStream(bs);
+            while (zs.getNextEntry() != null) {
+                String output = new String(zs.readAllBytes());
+                System.out.println(output);
+            }
+            zs.close();
+            bs.close();
+            
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
 
     }
 }
